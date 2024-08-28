0dfb5c44055e991b2f055941e4bf41224fe32d9f
OLUWATOBI OLUYOLE <105201994+tobyoluyole@users.noreply.github.com>
2024-08-28 16:48:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-const mongoose=require('mongoose')
 const express = require('express');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
 
-dotenv.config();
-
-// Initialize the app
 const app = express();
+const port = 3000;
 
-// Middleware
-app.use(express.json());
-
-// Initialize Sequelize
-const DB = process.env.DATABASE.replace(
-    '<password>',
-    process.env.DATABASE_PASSWORD,
-  );
-
-// Routes
-app.use('/api/users', userRoutes);
-
-// Error handling
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: err.message });
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-mongoose
-.connect(DB)
-.then(() => {
-  console.log('Database is connected');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-console.log(`App running on ${port}`);
-});
-process.on('unhandledRejection', (err) => {
-console.log(err.name, err.message);
-server.close(() => {
-  process.exit(1);
-})})
